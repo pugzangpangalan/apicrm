c6133164dc1f0403552024762fd9ae65df67d09f
MVP\jeriel.mercado <mercadojerielf@gmail.com>
2018-04-24 10:00:23 +0800

    Initial commit

--- src/main/java/com/apicrm/controller/ApiController.java
+++ src/main/java/com/apicrm/controller/ApiController.java
@@ -1,17 +1,23 @@
 package com.apicrm.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 @RequestMapping(value = "/api/crm")
 public class ApiController {
-	
-	
-	@RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
-	public ResponseEntity<?> generateKeyPair() {
-		return ResponseEntity.ok("Hello World");
+	Gson gson = new Gson();
+
+	@RequestMapping(value = "/helloWorld", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> sendEmailChangeCode(HttpSession session) {
+
+		return ResponseEntity.ok(gson.toJson("hello"));
 	}
 }
--- src/main/java/com/apicrm/main/ApiCrmApplication.java
+++ src/main/java/com/apicrm/main/ApiCrmApplication.java
@@ -2,8 +2,10 @@ package com.apicrm.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.apicrm")
 public class ApiCrmApplication {
 
 	public static void main(String[] args) throws Exception {
