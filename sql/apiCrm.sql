c211d6e080649603efdb71522e56fdcdac13a521
MVP\jeriel.mercado <mercadojerielf@gmail.com>
2018-04-24 11:14:08 +0800

    Add sql

---
+++ sql/apiCrm.sql
@@ -0,0 +1,50 @@
+/*
+TLC Table
+*/
+CREATE TABLE tlc(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+tlc_id VARCHAR(100),
+address VARCHAR(255),
+customer_name VARCHAR(100),
+phone_number VARCHAR(255),
+site_access_info VARCHAR(255),
+tlc_date DATE,
+time VARCHAR(5),
+scope_doc_sent INT,
+chorus_portal_order VARCHAR(100),
+icms_service_order VARCHAR(100),
+team_id INT,
+status INT,
+comments TEXT,
+schedule_once INT,
+scoping_doc_count INT,
+appointment_date DATE
+);
+
+/*
+PROJECT Table
+*/
+CREATE TABLE project(
+project_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+project_name VARCHAR(50)
+);
+
+/*
+TEAM Table
+*/
+
+CREATE TABLE team(
+team_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+project_id INT,
+team_name VARCHAR(100),
+FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE
+);
+
+/*
+status
+*/
+
+CREATE TABLE status (
+status_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+status_name VARCHAR(100)
+);
